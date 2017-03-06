c4a040638fca5ba075ded1743c1465fb63def293
Nilavan <nilavanrajamani@gmail.com>
2017-03-06 10:40:35 +0000

    Commit

--- AngularDemoApp/DemoApp/app/js/controllers/EventController.js
+++ AngularDemoApp/DemoApp/app/js/controllers/EventController.js
@@ -10,6 +10,29 @@ eventsApp.controller('EventController', function ($scope) {
             city: 'Mountain View',
             province: 'CA'
         },
-        imageUrl: '/img/angularjs-logo.png'
-    };
+        imageUrl: '/img/angularjs-logo.png',
+        sessions: [
+            {
+                name: 'Directives MasterClass',
+                createrName: 'Bob Smith',
+                duration: '1hr',
+                level: 'Advanced',
+                abstract: 'In this session you will learn the ins and outs of directive'
+            },
+            {
+                name: 'Scopes for fun and profit',
+                createrName: 'John Does',
+                duration: '1hr',
+                level: 'Introductory',
+                abstract: 'In this session will have closer look at the scopes'
+            },
+            {
+                name: 'Well Behaved Controllers',
+                createrName: 'Jane Does',
+                duration: '2hrs',
+                level: 'Intermediate',
+                abstract: 'In this session will have closer look at the scopes'
+            }
+        ]
+    }
 });
