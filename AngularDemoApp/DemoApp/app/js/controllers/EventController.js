927325ba7386a8c8954262707f7585948c37522a
Nilavan <nilavanrajamani@gmail.com>
2017-03-06 09:27:45 +0530

    zsf

--- AngularDemoApp/DemoApp/app/js/controllers/EventController.js
+++ AngularDemoApp/DemoApp/app/js/controllers/EventController.js
@@ -1,5 +1,15 @@
 ﻿'use strict';
 
 eventsApp.controller('EventController', function ($scope) {
-
+    $scope.event = {
+        name: 'Angular boot camp',
+        date: '1/1/89',
+        time: '10:30am',
+        location: {
+            address: 'Google Headquarters',
+            city: 'Mountain View',
+            province: 'CA'
+        },
+        imageUrl: '/img/angularjs-logo.png'
+    };
 });
